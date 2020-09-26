d7e4a9f80ee64db59ed5bb4235f6f0d4de02ff1b
Sagar <sagarthetennis10k@gmail.com>
2020-09-26 12:30:30 +0000

    all done

---
+++ website/local_setting.py
@@ -0,0 +1,15 @@
+SECRET_KEY = '1(t37smq!#fr)0h@ym^$^5*qn_z(i__&x#z&!bamwbk3vn6^tytennis'
+ALLOWED_HOSTS = ['206.189.143.249','sagarportfolio.com','www.sagarportfolio.com']   
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'djangodbman',
+        'PASSWORD': 'Tennisace95',
+        'HOST':'localhost',
+        'PORT':'5432',
+    }
+}
+
+DEBUG = False
+
